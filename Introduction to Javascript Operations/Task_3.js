ddca14d882ec988e3b2a8feca81ec5d0e809953b
Mahitosh Giri <mahitoshgiri287@gmail.com>
2023-11-16 23:30:30 +0530

    JS

---
+++ Introduction to Javascript Operations/Task_3.js
@@ -0,0 +1,30 @@
+//1)Unary Operators:-
+let num = 5;
+
+// Unary Negation (-)
+let negatedNum = -num; // Result: -5
+
+// Increment (++)
+let incrementedNum = ++num; // Result: 6
+
+// Decrement (--)
+let decrementedNum = --num; // Result: 5
+
+//2)Binary Operators:-
+let a = 5;
+let b = 2;
+
+// Addition (+)
+let sum = a + b; // Result: 7
+
+// Multiplication (*)
+let product = a * b; // Result: 10
+
+// Logical AND (&&)
+let result = (a > 0 && b > 0); // Result: true if both conditions are true
+
+//3)Ternary Operator:-
+let age = 20;
+
+// Ternary Operator
+let eligibility = (age >= 18) ? "Eligible" : "Not Eligible";
