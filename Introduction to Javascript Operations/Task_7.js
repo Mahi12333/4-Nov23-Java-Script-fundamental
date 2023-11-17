7230713716b58420fbee3a54c50662cfba9b6578
Mahitosh Giri <88604119+Mahi12333@users.noreply.github.com>
2023-11-17 15:44:22 +0530

    Update Task_7.js

--- Introduction to Javascript Operations/Task_7.js
+++ Introduction to Javascript Operations/Task_7.js
@@ -1,9 +1,3 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 
 // Function to calculate the area of a circle
 function calculateCircleArea(radius) {
@@ -12,29 +6,13 @@ function calculateCircleArea(radius) {
   return pi * radius * radius;
 }
 
-// Get user input using readline
-rl.question('Enter the radius of the circle: ', (radiusInput) => {
-  // Parse input to a number
-  const radius = parseFloat(radiusInput);
+// Example values
+let radius = 60; 
 
-  // Validate input
-  if (isNaN(radius) || radius <= 0) {
-    console.log('Please enter a valid numerical value for the radius.');
-  } else {
-    // Calculate the area of the circle
-    const area = calculateCircleArea(radius);
 
-    // Display the result
-    console.log(`Radius: ${radius}`);
-    console.log(`Area of the circle: ${area.toFixed(2)}`);
 
-    // Close the readline interface
-    rl.close();
-  }
-});
+// Calculate Area of circle 
+let interest = calculateCircleArea(radius);
 
-// Handle close event for readline interface
-rl.on('close', () => {
-  console.log('Program terminated.');
-  process.exit(0);
-});
+// Display the result
+console.log(`Radius: ${radius}`);
